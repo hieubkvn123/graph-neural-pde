de47c09bc816f468988fc71cb76d53d9e1c2e745
Minh Hieu Nong <mnong@virtuosgames.com>
2022-11-18 10:39:42 +0800

    Modified the ablation study script for multiple alphas

--- src/ablation_study_effect_of_alpha.py
+++ src/ablation_study_effect_of_alpha.py
@@ -32,7 +32,11 @@ for i, ds in enumerate(result_files.keys()):
                             mean_acc + std_acc, alpha=0.1, color=COLORS[j])
         ax[i].set_title(ds)
         ax[i].grid()
-        ax[i].legend(loc='upper right')
+
+        if(ds == 'Cora'):
+            ax[i].legend(loc='lower left')
+        else:
+            ax[i].legend(loc='upper right')
 
 print('Saving figure to tests/ablation_study_effect_of_alpha.png')
 fig.supxlabel('Depth (T)')
