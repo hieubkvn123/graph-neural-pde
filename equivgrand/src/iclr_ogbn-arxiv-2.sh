fe45c17a017f852b3a66a53e396e43a2212040aa
hieubkvn123 <hieubkvn123@gmail.com>
2022-11-26 15:10:05 +0700

    Checkpoint

---
+++ equivgrand/src/iclr_ogbn-arxiv-2.sh
@@ -0,0 +1,20 @@
+echo "python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split"
+
+python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split
+
+echo "python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split"
+
+python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split
+
+echo "python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split"
+
+python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split
+
+echo "python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split"
+
+python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split
+
+echo "python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split"
+
+python3 run_GNN.py --function ext_laplacian3 --block hard_attention --time 2 --alpha_ 1e-06 --epsilon_ 1e-03 --log_file 'tests/rebuttals/iclr_ogbn-arxiv_lowlabel.csv' --epoch 150 --max_iters 1000 --max_nfe 100000000 --l1_weight_decay 0.0 --decay 0.0001 --dataset ogbn-arxiv --threshold 69 --planetoid_split
+
